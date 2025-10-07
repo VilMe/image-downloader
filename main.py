0181aa928aa4ed1e11f55939ee1dac3251968c8d
VilMe <vilma.medrano00@gmail.com>
2025-10-06 21:23:52 -0500

    define download_image function, only first part defined

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def get_extension(image_url: str) -> str | None:
         if ext in image_url:
             return ext
         
+
+def download_image(image_url: str, name: str, folder: str = None):
+    if ext := get_extension(image_url):
+        if folder:
+            image_name: str = f'{folder}/{name}{ext}'
+        else:
+            image_name; str = f'{name}{ext}'
+    else:
+        
